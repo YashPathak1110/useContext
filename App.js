7d327769350369573673b634c826a8c99cc97054
Yash Pathak <144787943+YashPathak1110@users.noreply.github.com>
2023-09-20 16:58:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import A from './Componant/A';
+function App() {
+  return (
+    <div className="App">
+        <A/>
+    </div>
+  );
+}
+
+export default App;
